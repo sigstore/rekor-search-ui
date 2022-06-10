3c737e557656bbec6cec16b6d08d9e76189c830f
Philippe Deslauriers <philde@chainguard.dev>
2022-06-10 10:29:06 -0700

    Add fathom tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,30 @@
 import "../../styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
 function App({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		Fathom.load("EKFJYRUD", {
+			includedDomains: ["rekor.tlog.dev"],
+		});
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview();
+		}
+		// Record a pageview when route changes
+		router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+		// Unassign event listener
+		return () => {
+			router.events.off("routeChangeComplete", onRouteChangeComplete);
+		};
+	}, [router]);
+
 	return <Component {...pageProps} />;
 }
 
