81dd0aa107b9c08e81ca7cfff5fbe17f660a42a3
Phil Deslauriers <philde@chainguard.dev>
2022-06-14 09:43:45 -0700

    Add fathom tracking (#32)
    * Add fathom tracking
    
    * Switch to recommended NextJS config with fathom-client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import "../../styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { Router } from "next/router";
+import * as Fathom from "fathom-client";
+
+// Record a pageview when route changes.
+Router.events.on("routeChangeComplete", (as, routeProps) => {
+	if (!routeProps.shallow) {
+		Fathom.trackPageview();
+	}
+});
 
 function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		Fathom.load("EKFJYRUD", {
+			includedDomains: ["rekor.tlog.dev"],
+		});
+	}, []);
+
 	return <Component {...pageProps} />;
 }
 
