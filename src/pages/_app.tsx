b9343f6d090f4a3948224533c8ee9399b4888083
Philippe Deslauriers <philde@chainguard.dev>
2022-06-14 09:34:36 -0700

    Switch to recommended NextJS config with fathom-client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,28 +2,22 @@ import "../../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { Router } from "next/router";
 import * as Fathom from "fathom-client";
 
-function App({ Component, pageProps }: AppProps) {
-	const router = useRouter();
+// Record a pageview when route changes.
+Router.events.on("routeChangeComplete", (as, routeProps) => {
+	if (!routeProps.shallow) {
+		Fathom.trackPageview();
+	}
+});
 
+function App({ Component, pageProps }: AppProps) {
 	useEffect(() => {
 		Fathom.load("EKFJYRUD", {
 			includedDomains: ["rekor.tlog.dev"],
 		});
-
-		function onRouteChangeComplete() {
-			Fathom.trackPageview();
-		}
-		// Record a pageview when route changes
-		router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-		// Unassign event listener
-		return () => {
-			router.events.off("routeChangeComplete", onRouteChangeComplete);
-		};
-	}, [router]);
+	}, []);
 
 	return <Component {...pageProps} />;
 }
