7838f640870aa5a36c6236e2c977f5891a8965ec
Jason Hall <imjasonh@gmail.com>
2022-07-08 12:43:00 -0400

    Also parse certs in intoto entries (#34)
    * Also parse certs in intoto entries
    
    * yarn prettier -w *

---
+++ src/modules/components/Intoto.tsx
@@ -0,0 +1,71 @@
+import { Box, Link, Typography } from "@mui/material";
+import { dump } from "js-yaml";
+import NextLink from "next/link";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Intoto } from "../api/generated/types/intoto";
+import { decodex509 } from "../x509/decode";
+
+export function IntotoViewer({ intoto }: { intoto: Intoto }) {
+	const certContent = window.atob(intoto.publicKey || "");
+
+	const publicKey = {
+		title: "Public Key",
+		content: certContent,
+	};
+	if (certContent.includes("BEGIN CERTIFICATE")) {
+		publicKey.title = "Public Key Certificate";
+		publicKey.content = dump(decodex509(certContent), {
+			noArrayIndent: true,
+			lineWidth: -1,
+		});
+	}
+
+	return (
+		<Box>
+			<Typography
+				variant="h5"
+				sx={{ py: 1 }}
+			>
+				<NextLink
+					href={`/?hash=${intoto.content.hash?.algorithm}:${intoto.content.hash?.value}`}
+					passHref
+				>
+					<Link>Hash</Link>
+				</NextLink>
+			</Typography>
+
+			<SyntaxHighlighter
+				language="text"
+				style={atomDark}
+			>
+				{`${intoto.content.hash?.algorithm}:${intoto.content.hash?.value}`}
+			</SyntaxHighlighter>
+
+			<Typography
+				variant="h5"
+				sx={{ py: 1 }}
+			>
+				Signature
+			</Typography>
+			<SyntaxHighlighter
+				language="text"
+				style={atomDark}
+			>
+				{intoto.publicKey || ""}
+			</SyntaxHighlighter>
+			<Typography
+				variant="h5"
+				sx={{ py: 1 }}
+			>
+				{publicKey.title}
+			</Typography>
+			<SyntaxHighlighter
+				language="yaml"
+				style={atomDark}
+			>
+				{publicKey.content}
+			</SyntaxHighlighter>
+		</Box>
+	);
+}
