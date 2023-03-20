ab9d42076d5a497893f2cf27fe46dbe72c6c2349
Philippe Deslauriers <philde@chainguard.dev>
2023-03-20 13:11:24 -0700

    Remove fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,6 @@
-import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
-import { Router } from "next/router";
-import { useEffect } from "react";
-
-// Record a pageview when route changes.
-Router.events.on("routeChangeComplete", (as, routeProps) => {
-	if (!routeProps.shallow) {
-		Fathom.trackPageview();
-	}
-});
 
 function App({ Component, pageProps }: AppProps) {
-	useEffect(() => {
-		Fathom.load("EKFJYRUD", {
-			includedDomains: ["rekor.tlog.dev"],
-		});
-	}, []);
-
 	return <Component {...pageProps} />;
 }
 
