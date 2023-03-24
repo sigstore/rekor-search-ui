9b5fcaf4c5fc38a45a3ad7e1165bb114cf4a70c6
Trevor Rosen <trevrosen@github.com>
2023-03-24 13:45:26 -0500

    Remove base path from Next config
    Signed-off-by: Trevor Rosen <trevrosen@github.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-  basePath: '/rekor-search-ui' // remove once we have a custom domain
+	reactStrictMode: true
 };
 
 module.exports = nextConfig;
