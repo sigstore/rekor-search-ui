003a9990b61e0ed2ae39e11ac4a5f179b8d4d904
Bob Callaway <bcallaway@google.com>
2025-12-08 07:45:59 -0500

    try transpilePackages directive instead
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,7 @@ const config = {
 	testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
 
 	transformIgnorePatterns: [
-		"/node_modules/(?!(refractor|hastscript|unist-|vfile|property-information|space-separated-tokens|comma-separated-tokens|web-namespaces|html-void-elements|character-entities)/)",
+		"/node_modules/",
 		"^.+\\.module\\.(css|sass|scss)$",
 	],
 	verbose: true,
--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	transpilePackages: [
+	  "refractor",
+	  "hastscript",
+	  "hast-util-parse-selector",
+	  "hast-util-to-string",
+	  "unist-util-filter",
+	  "unist-util-visit",
+	  "unist-util-visit-parents",
+	  "unist-util-is",
+	  "vfile",
+	  "vfile-location",
+	  "property-information",
+	  "space-separated-tokens",
+	  "comma-separated-tokens",
+	  "web-namespaces",
+	  "html-void-elements",
+	  "character-entities",
+	],
 	output: "export",
 };
 
